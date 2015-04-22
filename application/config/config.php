110ace56dc398e78adc08799038dc9d3f80aa10d
Dhrumil Shah <dhrml.shah@gmail.com>
2015-04-23 01:24:26 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'https://cryptic-citadel-3874.herokuapp.com/';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
@@ -180,7 +180,7 @@ $config['directory_trigger']	= 'd'; // experimental not currently in use
 | your log files will fill up very fast.
 |
 */
-$config['log_threshold'] = 0;
+$config['log_threshold'] = 1;
 
 /*
 |--------------------------------------------------------------------------
