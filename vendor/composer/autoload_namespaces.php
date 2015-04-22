eb6812af9bd45474dd8314654191d2d810adaa8d
Dhrumil1808 <dhrml.shah@gmail.com>
2015-04-22 11:00:27 +0530

    commited

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,5 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'voku' => array($vendorDir . '/voku/portable-utf8/src', $vendorDir . '/voku/simple-cache/src', $vendorDir . '/voku/simple-mysqli/src'),
     'Monolog' => array($vendorDir . '/monolog/monolog/src'),
 );
